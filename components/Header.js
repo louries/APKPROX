f4787344f1c75ecb0a60a44286786beaab9d6c81
louries <98936209+louries@users.noreply.github.com>
2022-10-11 17:59:58 +0500

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,11 @@
-export default function Header({ title }) {
+export default function Header({ ApkProx }) {
   return (
     <>
       <h2 class="LC20lb MBeuO DKV0Md" style="text-align: center;"><span style="font-size: 18pt;">ApkProx - Free Download Android Mod Apk Games/Apps 2023</span></h2>
-<span style="font-size: 14pt;">We’re always adding new mods to our website so make sure to check us out regularly. We are the best source to download android <a href="https://apkprox.com/">Mod Apk Games</a> and premium apps.</span>
-
-<span style="font-size: 14pt;"><a href="https://apkprox.com">Apkprox.com</a> is an amazing site that provides modded games that allow you to enjoy the game, win, and level up quickly. That’s not all! We also share premium apps, which allow you to get more out of your premium account without having to spend any more money.</span>
-<h3 style="text-align: center;"><span style="font-size: 18pt;">Download Mod Apk Games &amp; Premium Apps For Android</span></h3>
-<span style="font-size: 14pt;">We have hundreds of <a href="https://apkprox.com/">MOD APK</a> games and apps, and we are sure that you'll find everything you're looking for on our website. We cover several genres, including racing, puzzle, action, and shooter games. The most famous games are <a href="https://apkprox.com/instander-apk/">Instander Mod Apk</a>, <a href="https://apkprox.com/shadow-fight-2-titan-mod-apk/">Shadow Fight 2 Titan Mod Apk</a>, <a href="https://apkprox.com/discovery-plus-premium-mod-apk/">Discovery Plus Mod Apk</a>, and <a href="https://apkprox.com/y3mate-apk-online-video-downloader-y2mate-alternative/">y3mate</a>.</span>
-
-<span style="font-size: 14pt;">You can also enjoy all of these cool new features, such as free ads, and unlimited resources like gems, and you can even unlock new skins and weapons. <strong><a href="https://apkprox.com">ApkProx</a></strong> is one of the fastest growing Mod publishing sites for android and ios mod games, keeping all files in a secure online repository and providing a hassle-free experience to game fans.</span>
-    </>
+<p>
+    We’re always adding new mods to our website so make sure to check us out regularly. We are the best source to download android Mod Apk Games and premium apps. 
+    Apkprox.com is an amazing site that provides modded games that allow you to enjoy the game, win, and level up quickly. That’s not all! We also share premium apps,
+    which allow you to get more out of your premium account without having to spend any more money. <a href="https://apkprox.com">Apkprox.com</a>
+   <p/>
   );
 }
