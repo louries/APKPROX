524f4e06aa0b82c2f645fac695e48de17867a170
louries <98936209+louries@users.noreply.github.com>
2022-10-11 17:50:17 +0500

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,26 +1,14 @@
 export default function Header({ title }) {
   return (
     <>
-      <h1 className="title">{title}</h1>
-      <p>
-        Hi 👋! This template gives you a{" "}
-        <a href="https://nextjs.org/">Next.js</a> app with the scaffolding for{" "}
-        <a href="https://www.netlify.com/products/functions/">
-          Netlify Functions
-        </a>
-        , <a href="https://www.netlify.com/products/forms/">Forms</a>, and{" "}
-        <a href="https://docs.netlify.com/routing/redirects/">Redirects</a>. Our
-        aim was to give you the code you would need to hit the ground running
-        with a few fun features.
-      </p>
+      <h2 class="LC20lb MBeuO DKV0Md" style="text-align: center;"><span style="font-size: 18pt;">ApkProx - Free Download Android Mod Apk Games/Apps 2023</span></h2>
+<span style="font-size: 14pt;">We’re always adding new mods to our website so make sure to check us out regularly. We are the best source to download android <a href="https://apkprox.com/">Mod Apk Games</a> and premium apps.</span>
 
-      <p>
-        You can find the code for this project on GitHub at{" "}
-        <a href="https://github.com/netlify-templates/nextjs-toolbox">
-          https://github.com/netlify-templates/nextjs-toolbox
-        </a>
-        ! Happy coding!
-      </p>
+<span style="font-size: 14pt;"><a href="https://apkprox.com">Apkprox.com</a> is an amazing site that provides modded games that allow you to enjoy the game, win, and level up quickly. That’s not all! We also share premium apps, which allow you to get more out of your premium account without having to spend any more money.</span>
+<h3 style="text-align: center;"><span style="font-size: 18pt;">Download Mod Apk Games &amp; Premium Apps For Android</span></h3>
+<span style="font-size: 14pt;">We have hundreds of <a href="https://apkprox.com/">MOD APK</a> games and apps, and we are sure that you'll find everything you're looking for on our website. We cover several genres, including racing, puzzle, action, and shooter games. The most famous games are <a href="https://apkprox.com/instander-apk/">Instander Mod Apk</a>, <a href="https://apkprox.com/shadow-fight-2-titan-mod-apk/">Shadow Fight 2 Titan Mod Apk</a>, <a href="https://apkprox.com/discovery-plus-premium-mod-apk/">Discovery Plus Mod Apk</a>, and <a href="https://apkprox.com/y3mate-apk-online-video-downloader-y2mate-alternative/">y3mate</a>.</span>
+
+<span style="font-size: 14pt;">You can also enjoy all of these cool new features, such as free ads, and unlimited resources like gems, and you can even unlock new skins and weapons. <strong><a href="https://apkprox.com">ApkProx</a></strong> is one of the fastest growing Mod publishing sites for android and ios mod games, keeping all files in a secure online repository and providing a hassle-free experience to game fans.</span>
     </>
   );
 }
